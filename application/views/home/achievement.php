45539523ed25955e6616d99f23eff0f1f3a2d1ba
mujibnesia <mujibnesiaa@gmail.com>
2022-12-01 14:11:58 +0700

    ganti achievement

---
+++ application/views/home/achievement.php
@@ -0,0 +1,44 @@
+    <!-- ======= achievement Section ======= -->
+    <section id="achievement" class="achievement">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>achievement</h2>
+        </div>
+
+        <div class="row gy-5">
+
+          <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
+            <div class="service-item">
+              <div class="img">
+                <img src="assets/img/achievement1.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="details position-relative">
+                <div class="icon">
+                  <i class="bi bi-trophy-fill"></i>
+                </div>
+                  <h3>JUARA 1</h3>
+                <p>Syamsuddin <br> Lomba Desain Poster Tingkat Regional <br> Di Universitas Trunojoyo Madura <br> Tahun 2021</p>
+              </div>
+            </div>
+          </div><!-- End achievement Item -->
+
+          <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
+            <div class="service-item">
+              <div class="img">
+                <img src="assets/img/achievement2.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="details position-relative">
+                <div class="icon">
+                  <i class="bi bi-trophy-fill"></i>
+                </div>
+                  <h3>JUARA 1</h3>
+                <p>Syamsuddin <br> Lomba Desain Poster Tingkat Provinsi <br> Di Universitas Negeri Surabaya <br> Tahun 2021</p>
+              </div>
+            </div>
+          </div><!-- End achievement Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End achievement Section -->
